d98893909ded6238af2879ab6372044aab79f721
sricharan-dev428 <sricharanburugu123@gmail.com>
2026-03-09 15:57:23 -0400

    feat: initialize Spring Boot project with base dependencies

---
+++ src/main/java/com/securedoc/ai/SecuredocAiApplication.java
@@ -0,0 +1,14 @@
+package com.securedoc.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredocAiApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(SecuredocAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securedoc/ai/SecuredocAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securedoc.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredocAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
